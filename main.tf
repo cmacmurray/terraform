69a97d32359314bfd3063aa66c5758919e2b58fd
clearmechris <chris@clearme.com>
2020-04-28 11:13:37 -0400

    updated with s3 backend

--- main.tf
+++ main.tf
@@ -1,29 +1,21 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# PIN TERRAFORM VERSION TO >= 0.12
-# The examples have been upgraded to 0.12 syntax
-# ---------------------------------------------------------------------------------------------------------------------
+terraform {
+  backend "s3" {
+  }
+}
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1"
+}
 
 terraform {
   required_version = ">= 0.12"
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
-# DEPLOY A S3 BUCKET WITH VERSIONING ENABLED INCLUDING TAGS
-# See test/terraform_aws_s3_example_test.go for how to write automated tests for this code.
-# ---------------------------------------------------------------------------------------------------------------------
 
 data "aws_iam_policy_document" "s3_bucket_policy" {
   statement {
     effect = "Allow"
     principals {
-      # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-      # force an interpolation expression to be interpreted as a list by wrapping it
-      # in an extra set of list brackets. That form was supported for compatibility in
-      # v0.11, but is no longer supported in Terraform v0.12.
-      #
-      # If the expression in the following list itself returns a list, remove the
-      # brackets to avoid interpretation as a list of lists. If the expression
-      # returns a single list item then leave it as-is and remove this TODO comment.
       identifiers = [local.aws_account_id]
       type        = "AWS"
     }
@@ -69,11 +61,6 @@ resource "aws_s3_bucket_policy" "bucket_access_policy" {
   bucket = aws_s3_bucket.test_bucket.id
   policy = data.aws_iam_policy_document.s3_bucket_policy.json
 }
-
-# ---------------------------------------------------------------------------------------------------------------------
-# LOCALS
-# Used to represent any data that requires complex expressions/interpolations
-# ---------------------------------------------------------------------------------------------------------------------
 
 data "aws_caller_identity" "current" {
 }

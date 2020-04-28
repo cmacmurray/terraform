05a2857f4cf022f93b44ea1847f30a3013d9dcdc
clearmechris <chris@clearme.com>
2020-04-28 14:01:44 -0400

    removed s3 bucket

--- main.tf
+++ main.tf
@@ -42,19 +42,6 @@ data "aws_iam_policy_document" "s3_bucket_policy" {
   }
 }
 
-resource "aws_s3_bucket" "test_bucket" {
-  bucket = "${local.aws_account_id}-${var.tag_bucket_name}"
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  tags = {
-    Name        = var.tag_bucket_name
-    Environment = var.tag_bucket_environment
-  }
-}
 
 resource "aws_s3_bucket_policy" "bucket_access_policy" {
   count  = var.with_policy ? 1 : 0

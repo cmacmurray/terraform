4ff8c5f7eeb71fe8e25dc6f12a84c101d781d50e
clearmechris <chris@clearme.com>
2020-04-28 20:50:23 -0400

    simple bucket

--- main.tf
+++ main.tf
@@ -11,48 +11,7 @@ terraform {
   required_version = ">= 0.12"
 }
 
-
-data "aws_iam_policy_document" "s3_bucket_policy" {
-  statement {
-    effect = "Allow"
-    principals {
-      identifiers = [local.aws_account_id]
-      type        = "AWS"
-    }
-    actions   = ["*"]
-    resources = ["${aws_s3_bucket.test_bucket.arn}/*"]
-  }
-
-  statement {
-    effect = "Deny"
-    principals {
-      identifiers = ["*"]
-      type        = "AWS"
-    }
-    actions   = ["*"]
-    resources = ["${aws_s3_bucket.test_bucket.arn}/*"]
-
-    condition {
-      test     = "Bool"
-      variable = "aws:SecureTransport"
-      values = [
-        "false",
-      ]
-    }
-  }
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket201901910"
+  acl    = "private"
 }
-
-
-resource "aws_s3_bucket_policy" "bucket_access_policy" {
-  count  = var.with_policy ? 1 : 0
-  bucket = aws_s3_bucket.test_bucket.id
-  policy = data.aws_iam_policy_document.s3_bucket_policy.json
-}
-
-data "aws_caller_identity" "current" {
-}
-
-locals {
-  aws_account_id = data.aws_caller_identity.current.account_id
-}
-

aa07ecae9541398769c81354b1955568b78ff60b
clearmechris <chris@clearme.com>
2020-04-28 21:23:00 -0400

    fix bucket name

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
   required_version = ">= 0.12"
 }
 
-resource "aws_s3_bucket" "409340534-034593-4050343-4x" {
+resource "aws_s3_bucket" "aaaa09340534-034593-4050343-4x" {
   bucket = "my-tf-test-bucket2019019109039393"
   acl    = "private"
 }
